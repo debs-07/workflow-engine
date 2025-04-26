78c0d975cc66a307d5db86df42206076141b423d
debs-07 <debakantasatapathy07@gmail.com>
2025-04-27 01:17:23 +0530

    Improve JWT error handling with specific checks for better user feedback and logging

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 
 import { CustomJwtPayload } from "../@types/jsonwebtoken/index.ts";
-import { AuthError } from "../utils/errors.util.ts";
+import { AuthError, CustomError } from "../utils/errors.util.ts";
 
 export const verifyJWT = (req: Request, _res: Response, next: NextFunction) => {
   const authHeader = req.header("Authorization");
@@ -13,10 +13,30 @@ export const verifyJWT = (req: Request, _res: Response, next: NextFunction) => {
     throw new AuthError("Token is invalid or expired");
   }
 
-  const decoded = jwt.verify(
-    token,
-    process.env.JWT_SECRET_KEY!
-  ) as CustomJwtPayload;
+  let decoded: CustomJwtPayload;
+
+  try {
+    decoded = jwt.verify(
+      token,
+      process.env.JWT_SECRET_KEY!
+    ) as CustomJwtPayload;
+  } catch (error: unknown) {
+    let errMessage: string;
+    let userMessage: string;
+
+    if (error instanceof jwt.TokenExpiredError) {
+      errMessage = "Token expired";
+      userMessage = "Your session has expired. Please log in again";
+    } else if (error instanceof jwt.JsonWebTokenError) {
+      errMessage = error.message;
+      userMessage = "Invalid token provided";
+    } else {
+      errMessage = "Unexpected JWT error";
+      userMessage = "Internal server error";
+    }
+
+    throw new CustomError(403, errMessage, userMessage);
+  }
 
   if (!decoded.userId) {
     throw new AuthError("Token is invalid or expired");
